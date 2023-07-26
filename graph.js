bc58e04ff5bf16023680b717085893ed43ab25eb
mingun1204 <133008080+mingun1204@users.noreply.github.com>
2023-07-26 01:24:55 +0000

    d

--- graph.js
+++ graph.js
@@ -1,20 +1,33 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const chartDataY = [80, 75, 62, 85, 55, 90, 78, 20, 74, 66];
-  const chartDataX = [0, 57, 114, 171, 228, 285, 342, 399, 456, 513];
+  const chartDataY = [80, 75, 62, 85, 55, 90, 78, 20, 74, 66, 80];
+  const chartDataX = [0, 57, 114, 171, 228, 285, 342, 399, 456, 490, 518];
+  const chartDataZ = [0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500];
   let chartData = '';
+  let chartDataz='';
 
   chartDataX.forEach((x, i) => {
     chartData += `${x},${chartDataY[i] * 2} `;
   });
+  chartDataZ.forEach((x, i) => {
+    chartDataz += `${x},${chartDataY[i] * 2} `;
+  });
 
   chartData = chartData.trim();
+  chartDataz = chartDataz.trim();
   const polylineElement = document.querySelector('.polyline1');
-
+  const polylineElement2 = document.querySelector('.polyline2');
   if (polylineElement !=null) {
     polylineElement.setAttribute('points', chartData);
+    polylineElement2.setAttribute('points', chartDataz);
     console.log("!!!!")
   } else {
     console.error('polyline element not found.');
   }
+  
 });
-// https://story.pxd.co.kr/1639 
+
+
+
+
+
+
